c45fd003b753a5bcd2cd7310486757444dfe68ea
CodeMaster1022 <CodeMaster.Han1022@gmail.com>
2024-06-07 23:52:32 +0900

    logo fix

--- src/components/logo/LogoMain.jsx
+++ src/components/logo/LogoMain.jsx
@@ -1,5 +1,5 @@
 // material-ui
-
+import useMediaQuery from '@mui/material/useMediaQuery';
 // project-import
 /**
  * if you want to use image instead of <svg> uncomment following.
@@ -12,6 +12,7 @@
 // ==============================|| LOGO SVG ||============================== //
 import logo from 'assets/images/icons/Logo-2.svg';
 export default function LogoMain() {
+  const downLG = useMediaQuery((theme) => theme.breakpoints.down('lg'));
   return (
     /**
      * if you want to use image instead of svg uncomment following, and comment out <svg> element.
@@ -19,8 +20,6 @@ export default function LogoMain() {
      * <img src={theme.palette.mode === ThemeMode.DARK ? logoDark : logo} alt="Mantis" width="100" />
      *
      */
-    <>
-      <img src={logo} alt="Mantis" width="250" />
-    </>
+    <>{downLG ? <img src={logo} alt="Mantis" width="220" /> : <img src={logo} alt="Mantis" width="275" />}</>
   );
 }
